b19b0297a52c1a4194821bfc5ee062d44af76f8d
Skyler Rexroad <skyrex1095@gmail.com>
2016-04-30 16:34:44 -0600

    Added more to generation

--- js/generate.js
+++ js/generate.js
@@ -1,17 +1,39 @@
 $(document).ready(function() {
 	regenerate();
+	$("#date").text(new Date().getFullYear())
 });
 
 function generate() {
 	bits = {
-		"nouns": ["tree", "trucker", "soldier", "girl", "woman", "boy", "man"],
-		"adjectives": ["space", "sad", "solemn", "underdog", "little", "old", "young"]
+		"gamemodifier": ["mmo", "retro", "pixelated", "grimy", "gritty", "brown", "colorful", "cheerful", "macabre", "noir", "cartoony"],
+		"gametype": ["rpg", "platformer", "sidescroller", "first-person shooter", "third-person shooter", "roguelike", "metroidvania", "puzzle game", "simulator", "hack n' slash"],
+		"nouns": ["truckers", "soldier", "girl", "woman", "boy", "man"],
+		"adjectives": ["space", "sad", "solemn", "underdog", "little", "old", "young"],
+		"settings": ["a forest", "the ocean", "space", "an underground cave system", "a big city", "a small town", "the desert", "an island"]
 	};
 
+	sentence = "";
+
+	gamemodifier = bits.gamemodifier[rand = getRandomInt(0, bits.gamemodifier.length)];
+	gametype = bits.gametype[rand = getRandomInt(0, bits.gametype.length)];
 	noun = bits.nouns[rand = getRandomInt(0, bits.nouns.length)];
 	adjective = bits.adjectives[rand = getRandomInt(0, bits.adjectives.length)];
+	adjective2 = bits.adjectives[rand = getRandomInt(0, bits.adjectives.length)];
+	setting = bits.settings[rand = getRandomInt(0, bits.settings.length)];
 
-	return "A " + adjective + " " + noun + "."
+	if (adjective == adjective2) {
+		adjective2 = bits.adjectives[rand = getRandomInt(0, bits.adjectives.length)];
+	}
+
+	if (getRandomInt(0, 100) > 50) {
+		sentence = gamemodifier + " " + gametype + " about " + " " + adjective + " " + noun + " in " + setting;
+	} else {
+		sentence = gamemodifier + " " + gametype + " about " + " " + adjective + " " + adjective2 + " " + noun + " in " + setting;
+	}
+
+	firstLetter = sentence.charAt(0).toUpperCase();
+	sentence = firstLetter + sentence.substring(1, sentence.length) + ".";
+	return sentence;
 }
 
 function regenerate() {
