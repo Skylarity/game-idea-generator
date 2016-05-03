2aa63505e771fa1f18577bf2a21363de3b676110
Skyler Rexroad <skyrex1095@gmail.com>
2016-05-03 11:46:05 -0600

    Added a neutral game modifier

--- js/generate.js
+++ js/generate.js
@@ -7,7 +7,7 @@ function generate() {
 	bits = {
 		"happyGameModifiers": ["colorful", "cheerful", "cartoony", "heart-warming"],
 		"sadGameModifiers": ["gritty", "grimy", "brown", "macabre", "noir", "dark"],
-		"neutralGameModifiers": ["thrilling"],
+		"neutralGameModifiers": ["thrilling", "mysterious"],
 		"gameTypes": ["MMO", "artsy", "pixelated", "retro", "bullethell", "physics-based"],
 
 		"gameGenres": ["RPG", "platformer", "sidescroller", "first-person game", "third-person game", "top-down game", "tower defense game", "puzzle game", "simulator", "hack n' slash", "roguelike", "metroidvania"],
